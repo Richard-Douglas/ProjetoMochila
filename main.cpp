932708ebb1155cb84fae67d27c50b16c3d3659e2
Richard Douglas <83971481+Richard-Douglas@users.noreply.github.com>
2023-09-19 19:53:27 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <cmath>
 #include <SOIL/SOIL.h>
 
+// Estrutura do objeto
 struct Object {
     float x, y, size;
     int weight, value;
@@ -16,12 +17,12 @@ struct Object {
     GLuint textureId;
 };
 
-Object objects[10];
-float player_x = 0.0f, player_y = 0.0f;
+Object objects[10]; // Número de objetos
+float player_x = 0.0f, player_y = 0.0f; // Posição inicial do personagem
 float currentWeight = 0.0f; // Peso atual na mochila
 float currentValue = 0.0f; // Valor atual na mochila
 const float maxWeight = 50.0f; // Peso máximo
-int textCarreg = 0;
+int textCarreg = 0; // Verificar se as texturas já foram carregadas
 
 bool checkOverlap(Object a, Object b, float minDistance) {
     float distance = std::sqrt(std::pow(a.x - b.x, 2) + std::pow(a.y - b.y, 2));
@@ -40,19 +41,28 @@ public:
     float totalValue = 0.0f; // Novo: Valor total dos itens coletados
     GLuint backgroundTexture;
     GLuint objectTextures[3];
+    GLuint characterTexture;
 
     MyGlWindow(int X, int Y, int W, int H, const char *L = 0) : Fl_Gl_Window(X, Y, W, H, L) {
     }
 
 	void loadTextures() {
 		if(textCarreg == 0){
+			if (!characterTexture) {
+				characterTexture = SOIL_load_OGL_texture("personagem.png", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+				if (!characterTexture) {
+				    printf("SOIL loading error: '%s'\n", SOIL_last_result());
+				}
+			}
+
+		
 			if (!backgroundTexture) {
 				backgroundTexture = SOIL_load_OGL_texture("background.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
 				if (!backgroundTexture) {
 				    printf("SOIL loading error: '%s'\n", SOIL_last_result());
 				}
 			}
-			// Load textures for objects here, similarly
+			// Carrega as textura para cada objeto
 			for (int i = 0; i < 10; ++i) {
 		        if (!objects[i].collected) {
 		            drawObject(objects[i], i);
@@ -98,6 +108,7 @@ public:
 		}
 	}
 
+	// Ativa o OpenGl
 	void setupOpenGL() {
 		glEnable(GL_BLEND);  
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -109,6 +120,7 @@ public:
 		glClear(GL_COLOR_BUFFER_BIT);
 	}
 
+	// Carrega a textura do background
 	void drawBackground() {
 		glEnable(GL_TEXTURE_2D);
 		glBindTexture(GL_TEXTURE_2D, backgroundTexture);
@@ -122,10 +134,11 @@ public:
 		glDisable(GL_TEXTURE_2D);
 	}
 
+	// Desenha o todos os objetos
 	void drawObjects() {
 		for (int i = 0; i < 10; ++i) {
 		    if (!objects[i].collected) {
-		        drawObject(objects[i], i); // Assume drawObject handles texture binding
+		        drawObject(objects[i], i);
 		    }
 		}
 	}
@@ -139,70 +152,38 @@ public:
             gl_draw(ssInfo.str().c_str(), -1.45f, 1.4f - i * 0.1f);
         }
         
-        // Novo: Draw backpack information
+        // Escreve as informações da mochila
         std::stringstream ssBackpack;
         ssBackpack << "Peso atual: " << currentWeight << "/" << maxWeight << " (" << (currentWeight / maxWeight) * 100 << "%)";
         ssBackpack << " ; R$->" << totalValue;
         glColor3f(1.0, 1.0, 1.0);
-       	gl_font(FL_HELVETICA, 12);
+       	gl_font(FL_HELVETICA, 16);
         gl_draw(ssBackpack.str().c_str(), -1.4f, -1.4f);
 	}
 
+	// Desenha a textura do personagem
 	void drawCharacter() {
-		// Draw character
-        // Body (rectangle)
-        glColor3f(0.0f, 0.0f, 1.0f);
-        glBegin(GL_QUADS);
-        glVertex2f(player_x, player_y);
-        glVertex2f(player_x + 0.1f, player_y);
-        glVertex2f(player_x + 0.1f, player_y + 0.2f);
-        glVertex2f(player_x, player_y + 0.2f);
-        glEnd();
+		// Ativar textura
+		glEnable(GL_BLEND);
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, characterTexture);
 
-        // Head (circle)
-        glColor3f(1.0f, 1.0f, 1.0f);
-        glBegin(GL_TRIANGLE_FAN);
-        glVertex2f(player_x + 0.05f, player_y + 0.25f);
-        for (int i = 0; i <= 360; i += 10) {
-            float theta = i * 3.14159 / 180;
-            glVertex2f(player_x + 0.05f + std::cos(theta) * 0.05, player_y + 0.25f + std::sin(theta) * 0.05);
-        }
-        glEnd();
+		// Desenhar corpo do personagem
+		glColor3f(1.0f, 1.0f, 1.0f);
+		glBegin(GL_QUADS);
+		glTexCoord2f(0.0, 0.0); glVertex2f(player_x, player_y);
+		glTexCoord2f(1.0, 0.0); glVertex2f(player_x + 0.3f, player_y);
+		glTexCoord2f(1.0, 1.0); glVertex2f(player_x + 0.3f, player_y + 0.6f);
+		glTexCoord2f(0.0, 1.0); glVertex2f(player_x, player_y + 0.6f);
+		glEnd();
+		
+		// Desativar textura
+		glDisable(GL_BLEND);
+		glDisable(GL_TEXTURE_2D);
 
-        // Arms (rectangles)
-        glColor3f(1.0f, 0.0f, 0.0f);
-        glBegin(GL_QUADS);
-        glVertex2f(player_x - 0.05f, player_y + 0.15f);
-        glVertex2f(player_x, player_y + 0.15f);
-        glVertex2f(player_x, player_y + 0.05f);
-        glVertex2f(player_x - 0.05f, player_y + 0.05f);
-        glEnd();
-
-        // Other arm
-        glBegin(GL_QUADS);
-        glVertex2f(player_x + 0.1f, player_y + 0.15f);
-        glVertex2f(player_x + 0.15f, player_y + 0.15f);
-        glVertex2f(player_x + 0.15f, player_y + 0.05f);
-        glVertex2f(player_x + 0.1f, player_y + 0.05f);
-        glEnd();
-
-        // Legs (rectangles)
-        glColor3f(0.0f, 1.0f, 0.0f);
-        glBegin(GL_QUADS);
-        glVertex2f(player_x, player_y);
-        glVertex2f(player_x + 0.025f, player_y);
-        glVertex2f(player_x + 0.025f, player_y - 0.1f);
-        glVertex2f(player_x, player_y - 0.1f);
-        glEnd();
-
-        glBegin(GL_QUADS);
-        glVertex2f(player_x + 0.075f, player_y);
-        glVertex2f(player_x + 0.1f, player_y);
-        glVertex2f(player_x + 0.1f, player_y - 0.1f);
-        glVertex2f(player_x + 0.075f, player_y - 0.1f);
-        glEnd();
 	}
 
+	// Verifica colissões
 	void handleCollisions() {
 		for (int i = 0; i < 10; ++i) {
             if (!objects[i].collected && checkCollision(player_x, player_y, objects[i].x, objects[i].y, objects[i].size)) {
@@ -215,6 +196,7 @@ public:
         }
 	}
 
+	// Opera todas as funções de desenho
 	void draw() {
 		loadTextures();
 		setupOpenGL();
@@ -226,13 +208,11 @@ public:
 		glDisable(GL_BLEND);
 	}
 
-    // ... (o resto do seu código para o método drawObject)
-
     void drawObject(Object obj, int index) {
     	glEnable(GL_BLEND);
         glEnable(GL_TEXTURE_2D);
 		glBindTexture(GL_TEXTURE_2D, obj.textureId);
-		glColor3f(1.0f, 1.0f, 1.0f); // Defina a cor como branco quando usar texturas
+		glColor3f(1.0f, 1.0f, 1.0f);
 		glBegin(GL_QUADS);
 		glTexCoord2f(0.0, 0.0); glVertex2f(obj.x, obj.y);
 		glTexCoord2f(1.0, 0.0); glVertex2f(obj.x + obj.size, obj.y);
@@ -243,15 +223,17 @@ public:
 		glDisable(GL_BLEND);
 		glDisable(GL_TEXTURE_2D);
 
-        // Draw weight below the object
+        // Escreve os pesos dos objetos
 		std::stringstream ssWeight;
 		ssWeight << obj.weight << "kg | R$ " << obj.value;
-		glColor3f(1.0, 1.0, 1.0); // White color for the text
-		gl_font(FL_HELVETICA, 16); // Set the font and size
-		gl_draw(ssWeight.str().c_str(), static_cast<float>(obj.x + 0.02), static_cast<float>(obj.y - 0.08)); // Draw the text slightly offset from the object
+		glColor3f(1.0, 1.0, 1.0); // Cor do texto
+		gl_font(FL_HELVETICA, 16); // Fonte e tamanho
+		gl_draw(ssWeight.str().c_str(), static_cast<float>(obj.x + 0.02), static_cast<float>(obj.y - 0.08)); // Posição do texto
 
     }
     
+    
+    // Inicializa os objetos
     void initializeObjects() {
         std::srand(std::time(0));
         for (int i = 0; i < 10; ++i) {
@@ -261,7 +243,7 @@ public:
 		        objects[i].x = std::rand() % 200 / 100.0 - 1.0;
 		        objects[i].y = std::rand() % 200 / 100.0 - 1.0;
 		        for (int j = 0; j < i; ++j) {
-		            if (checkOverlap(objects[i], objects[j], 0.2)) {
+		            if (checkOverlap(objects[i], objects[j], 0.4)) {
 		                overlap = true;
 		                break;
 		            }
@@ -271,14 +253,6 @@ public:
 		    objects[i].size =  0.25;
 		    objects[i].weight = (i + 1) * 2 + std::rand() % 5;
 		    objects[i].value = std::rand() % 100 + 10;
-
-		    if (i < 4) {
-		        objects[i].shape = 0;
-		    } else if (i < 7) {
-		        objects[i].shape = 1;
-		    } else {
-		        objects[i].shape = 2;
-		    }
 		}
     }
 
@@ -287,20 +261,20 @@ int main(int argc, char **argv) {
     Fl_Window win(800, 600, "O Problema da Mochila");
     MyGlWindow mygl(10, 10, 780, 580);
 
-    mygl.initializeObjects();  // Initialize the objects
+    mygl.initializeObjects();
     win.end();
     win.show(argc, argv);
 
-    float totalValue = 0.0f; // Novo: Total value in the backpack
+    float totalValue = 0.0f;
 
     while (Fl::check()) {
-		// Move character with keys
+		// Move de acordo com a tecla
 		if (Fl::event_key('w') && player_y + 0.2f < 1.5) player_y += 0.01f;
 		if (Fl::event_key('s') && player_y > -1.5) player_y -= 0.01f;
 		if (Fl::event_key('a') && player_x > -1.5) player_x -= 0.01f;
 		if (Fl::event_key('d') && player_x + 0.1f < 1.5) player_x += 0.01f;
 
-        // Novo: Update total value in the backpack
+        // Atualiza o valor da mochila
         totalValue = 0.0f;
         for (int i = 0; i < 10; ++i) {
             if (objects[i].collected) {
